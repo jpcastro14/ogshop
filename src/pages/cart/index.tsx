ad3cd5bae4cbf82f4e4133a0985d53e6e01fb83a
João Pedro Ferreira de Castro <joao.ferreira.castro@senado.leg.br>
2025-10-06 19:21:15 -0300

    Feat: add responsive layout to cart page

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,96 +1,78 @@
-import { BsCartPlus } from "react-icons/bs";
+import { BsTrash, BsPlus } from "react-icons/bs";
 
 export function Cart() {
   return (
     <>
-      <div className="w-full max-w-7xl flex items-start border-b-2 border-b-green-400  justify-self-center m-10 px-5">
-        Mais vendidos
+      <div className="max-w-7xl border-b-2 border-b-green-400 flex items-start mx-auto mt-10 m-10 px-5 ">
+        <h2 className="font-semi-bold text-2xl ">Seu carrinho</h2>
       </div>
-      <div className="w-full max-w-7xl grid gap-5 justify-self-center mt-10 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 px-10">
-        <section className=" relative shadow-sm min-w-72 max-w-lg w-full justify-items-center rounded-md mx-auto mb-2.5 hover scale-100 ">
-          <img
-            className="w-50 min-h-50"
-            src="https://media.istockphoto.com/id/182838201/pt/foto/margarida-no-branco-com-tra%C3%A7ado-de-recorte.jpg?s=2048x2048&w=is&k=20&c=8YFMCqQ3iGyOsdC-H-TYMIlX2lXYeJM-OqztExckZFQ="
-          />
-          <p>Girassol 10g - Safra Julho 2025</p>
-          <div className="w-full flex flex-row my-5 px-5 gap-2.5 text-center">
-            <s className=" rounded bg-red-300 text-white p-0.5 ">R$:28.00</s>
-            <p className=" rounded w-full bg-green-500 text-white p-0.5 hover:bg-green-700 transition 300ms ">
-              R$:25.00 - 1 Grama
-            </p>
-          </div>
-          <div className="w-full px-5 mb-5 ">
-            <button className="w-full">
-              <BsCartPlus className="border  rounded w-full h-8 py-0.5" />
-            </button>
-          </div>
-          <span className="absolute bg-amber-400 px-2.5 rounded-full w-6 h-6 justify-center align-middle flex top-4 right-4 ">
-            %
-          </span>
-        </section>
-        <section className=" relative shadow-sm min-w-72 max-w-lg w-full justify-items-center rounded-md mx-auto mb-2.5 hover scale-100 ">
-          <img
-            className="w-50 max-h-auto"
-            src="https://media.istockphoto.com/id/182838201/pt/foto/margarida-no-branco-com-tra%C3%A7ado-de-recorte.jpg?s=2048x2048&w=is&k=20&c=8YFMCqQ3iGyOsdC-H-TYMIlX2lXYeJM-OqztExckZFQ="
-          />
-          <p>Girassol 10g - Safra Julho 2025</p>
-          <div className="w-full flex flex-row my-5 px-5 gap-2.5 text-center">
-            <s className=" rounded bg-red-300 text-white p-0.5 ">R$:28.00</s>
-            <p className=" rounded w-full bg-green-500 text-white p-0.5 hover:bg-green-700 transition 300ms ">
-              R$:25.00 - 1 Grama
-            </p>
-          </div>
-          <div className="w-full px-5 mb-5 ">
-            <button className="w-full">
-              <BsCartPlus className="border  rounded w-full h-8 py-0.5" />
-            </button>
-          </div>
-          <span className="absolute bg-amber-400 px-2.5 rounded-full w-6 h-6 justify-center align-middle flex top-4 right-4 ">
-            %
-          </span>
-        </section>
-        <section className=" relative shadow-sm min-w-72 max-w-lg w-full justify-items-center rounded-md mx-auto mb-2.5 hover scale-100 ">
-          <img
-            className="w-50 max-h-auto"
-            src="https://media.istockphoto.com/id/182838201/pt/foto/margarida-no-branco-com-tra%C3%A7ado-de-recorte.jpg?s=2048x2048&w=is&k=20&c=8YFMCqQ3iGyOsdC-H-TYMIlX2lXYeJM-OqztExckZFQ="
-          />
-          <p>Girassol 10g - Safra Julho 2025</p>
-          <div className="w-full flex flex-row my-5 px-5 gap-2.5 text-center">
-            <s className=" rounded bg-red-300 text-white p-0.5 ">R$:28.00</s>
-            <p className=" rounded w-full bg-green-500 text-white p-0.5 hover:bg-green-700 transition 300ms ">
-              R$:25.00 - 1 Grama
-            </p>
-          </div>
-          <div className="w-full px-5 mb-5 ">
-            <button className="w-full">
-              <BsCartPlus className="border  rounded w-full h-8 py-0.5" />
-            </button>
-          </div>
-          <span className="absolute bg-amber-400 px-2.5 rounded-full w-6 h-6 justify-center align-middle flex top-4 right-4 ">
-            %
-          </span>
-        </section>
-        <section className=" relative shadow-sm min-w-72 max-w-lg w-full justify-items-center rounded-md mx-auto mb-2.5 hover scale-100 ">
-          <img
-            className="w-50 max-h-auto"
-            src="https://media.istockphoto.com/id/182838201/pt/foto/margarida-no-branco-com-tra%C3%A7ado-de-recorte.jpg?s=2048x2048&w=is&k=20&c=8YFMCqQ3iGyOsdC-H-TYMIlX2lXYeJM-OqztExckZFQ="
-          />
-          <p>Girassol 10g - Safra Julho 2025</p>
-          <div className="w-full flex flex-row my-5 px-5 gap-2.5 text-center">
-            <s className=" rounded bg-red-300 text-white p-0.5 ">R$:28.00</s>
-            <p className=" rounded w-full bg-green-500 text-white p-0.5 hover:bg-green-700 transition 300ms ">
-              R$:25.00 - 1 Grama
-            </p>
-          </div>
-          <div className="w-full px-5 mb-5 ">
-            <button className="w-full">
-              <BsCartPlus className="border  rounded w-full h-8 py-0.5" />
-            </button>
-          </div>
-          <span className="absolute bg-amber-400 px-2.5 rounded-full w-6 h-6 justify-center align-middle flex top-4 right-4 ">
-            %
-          </span>
-        </section>
+
+      <div id="Container" className="max-w-7xl items-center mx-auto">
+        <div
+          id="Cart_item"
+          className="max-w-7xl border-slate-200 border rounded shadow flex items-start mx-2 mt-10 p-3"
+        >
+          <section className="lg:flex lg:flex-row  w-full flex flex-col items-center">
+            <img
+              className="max-w-40 shadow rounded-full"
+              src="https://media.istockphoto.com/id/182838201/pt/foto/margarida-no-branco-com-tra%C3%A7ado-de-recorte.jpg?s=2048x2048&w=is&k=20&c=8YFMCqQ3iGyOsdC-H-TYMIlX2lXYeJM-OqztExckZFQ="
+            />
+
+            <div id="initial_info" className="w-36 lg:w-full lg:ml-10 ">
+              <h1 className="text-4xl ">Girassol</h1>
+              <p className="bg-green-400 px-2 rounded">Safra Maio - 2025</p>
+            </div>
+
+            <div
+              id="secondary_info"
+              className="m-6 w-full lg:max-w-80 rounded  lg:p-2 lg:shadow"
+            >
+              <h1 className="text-4xl text-start ">Girassol</h1>
+              <span className="w-full text-start border-b border-green-400">
+                R$: 25 / Grama
+              </span>
+              <p className=" text-start font-bold mt-2">
+                Total: R$:100 - 4 Gramas
+              </p>
+            </div>
+            <div className=" border-slate-200 p-2 border rounded-full flex flex-row justify-between w-full lg:flex-col lg:w-auto lg:h-32  ">
+              <BsTrash size={30} color="red" className="lg:order-last" />
+              <BsPlus size={30} color="blue" className="lg:order-first" />
+            </div>
+          </section>
+        </div>
+        <div
+          id="Cart_item"
+          className="max-w-7xl border-slate-200 border rounded shadow flex items-start mx-2 mt-10 p-3"
+        >
+          <section className="lg:flex lg:flex-row  w-full flex flex-col items-center">
+            <img
+              className="max-w-40 shadow rounded-full"
+              src="https://media.istockphoto.com/id/182838201/pt/foto/margarida-no-branco-com-tra%C3%A7ado-de-recorte.jpg?s=2048x2048&w=is&k=20&c=8YFMCqQ3iGyOsdC-H-TYMIlX2lXYeJM-OqztExckZFQ="
+            />
+            <div id="initial_info" className="w-36 lg:w-full lg:ml-5 ">
+              <h1 className="text-4xl ">Girassol</h1>
+              <p className="bg-green-400 px-2 rounded">Safra Maio - 2025</p>
+            </div>
+
+            <div
+              id="secondary_info"
+              className="m-6 w-full lg:max-w-80 rounded  lg:p-2 lg:shadow"
+            >
+              <h1 className="text-4xl text-start ">Girassol</h1>
+              <span className="w-full text-start border-b border-green-400">
+                R$: 25 / Grama
+              </span>
+              <p className=" text-start font-bold mt-2">
+                Total: R$:100 - 4 Gramas
+              </p>
+            </div>
+            <div className=" border-slate-200 p-2 border rounded-full flex flex-row justify-between w-full lg:flex-col lg:w-auto lg:h-32  ">
+              <BsTrash size={30} color="red" className="lg:order-last" />
+              <BsPlus size={30} color="blue" className="lg:order-first" />
+            </div>
+          </section>
+        </div>
       </div>
     </>
   );
